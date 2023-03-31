3086d504566dbb3e9da3b02964acec47c15eabed
Fabian <ytfabiskora07@gmail.coml>
2023-03-31 20:47:32 +0200

    First commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forum Informatyka na wypasie</title>
+</head>
+<body>
+    <!--
+        Plan szkieletu HTML i CSS 
+        1. Logo w Gimp
+        2. Wyglad strony(kolor tla itd.)
+        3. Navbar
+        4. Formularz rejestracyjny
+        5. Newsletter
+    -->
+</body>
+</html>
